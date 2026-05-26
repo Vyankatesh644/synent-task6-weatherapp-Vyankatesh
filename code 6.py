43d8ddd2c49d83c14bf04043779766b587dcf053
Vyankatesh Kulkarni <162884282+Vyankatesh644@users.noreply.github.com>
2026-05-26 22:23:04 +0530

    Add files via upload

---
+++ code 6.py
@@ -0,0 +1,37 @@
+import requests
+
+# Enter your API key here
+API_KEY = "fb4493620d73b233e5ff19c72e5ab947"
+
+# User input
+city = input("Enter city name: ")
+
+# API URL
+url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={"fb4493620d73b233e5ff19c72e5ab947"}&units=metric"
+
+try:
+    # Send request
+    response = requests.get(url)
+
+    # Convert response to JSON
+    data = response.json()
+
+    # Check if city exists
+    if data["cod"] != 200:
+        print("City not found!")
+
+    else:
+        # Extract weather details
+        temperature = data["main"]["temp"]
+        humidity = data["main"]["humidity"]
+        weather = data["weather"][0]["description"]
+
+        # Display output
+        print("\n=== Weather Report ===")
+        print("City:", city)
+        print("Temperature:", temperature, "°C")
+        print("Humidity:", humidity, "%")
+        print("Condition:", weather)
+
+except Exception as e:
+    print("Error:", e)
